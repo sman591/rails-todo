92c0a9eeff840a9bae7cd42c35eab2c02369adb3
Stuart Olivera <stuart@stuartolivera.com>
2013-06-25 15:45:45 -0400

    Add capability to check/uncheck all items on page, refactor JavaScript surrounding it
    Signed-off-by: Stuart Olivera <stuart@stuartolivera.com>

--- app/assets/javascripts/checkboxes.js
+++ app/assets/javascripts/checkboxes.js
@@ -1,37 +1,74 @@
 (function() {
 
   $().ready(function(){
+
     $('.regular-checkbox').change(function() {
-      var id = $("#" + $(this).attr('id') + "_id").val();
-      var completed; 
-      if ($(this).is(':checked')) {
-        completed = '1';
-      } else {
-        completed = '0';
-      } 
-      $.ajax({
-          type: "PUT",
-          url: "/todo_items/complete/" + id,
-          data: { completed: completed }
-       });
-       sync_checkbox(this);     
+      checkbox_clicked(this); 
     });
+
+    $('.check-all').click(function() {
+      checkbox_change_all('check');
+    });
+    $('.uncheck-all').click(function() {
+      checkbox_change_all('uncheck');
+    });
+
   });
 
   function current_url(num) {
-    
+
     var path = window.location.pathname.split('/');
     
     return path[num];
     
   }
 
-  function sync_checkbox(checkbox) {
+  function checkbox_clicked(checkbox) {
+
+    /* Handles when checkboxes get clicked */
+
+    var completed; 
+    if ($(checkbox).is(':checked')) {
+      completed = '1';
+    } else {
+      completed = '0';
+    }
+
+    checkbox_change(checkbox, completed);
+
+  }
+
+  function checkbox_change(checkbox, completed) {
+
+    /* Submit request to server */
+
+    var id = $("#" + $(checkbox).attr('id') + "_id").val();
+    
+    if (id == undefined) {
+      return;
+    }
+
+    $.ajax({
+        type: "PUT",
+        url: "/todo_items/complete/" + id,
+        data: { completed: completed }
+     });
+
+     sync_checkbox(checkbox, completed); 
+
+  }
+
+  function sync_checkbox(checkbox, completed) {
+
+    /* Updates checkbox & title on page to reflect correct checkmark */
 
     var fadeTime = 800;
 
-    if ($(checkbox).is(':checked')) {
+    if (completed == 1) {
       
+      if (!$(checkbox).is(":checked"))
+        $(checkbox).prop('checked', true);
+
       if ($(checkbox).parent().parent().is('tr'))
         $title = $(checkbox).parent().parent().find('td:nth-child(2) a');
       else
@@ -48,8 +85,11 @@
       }
     
     }
-    else {
+    else if (completed == 0) {
     
+      if ($(checkbox).is(":checked"))
+        $(checkbox).prop('checked', false);
+
       if ($(checkbox).parent().parent().is('tr'))
         $title = $(checkbox).parent().parent().find('td:nth-child(2) a.complete');
       else
@@ -66,6 +106,33 @@
       }
     
     }
+
+  }
+
+  function checkbox_change_all(action) {
+
+    /* Take all of the items on the page and mark them checked/unchecked, process the request to the server */
+
+    switch (action) {
+
+      case 'check':
+        completed = 1;
+      break;
+      case 'uncheck':
+        completed = 0;
+      break;
+      default: 
+        alert('Invalid action: ' + action);
+        return;
+      break;
+
+    }
+
+    $('table.todo-list > tbody > tr').each(function() {
+
+      checkbox_change($(this).find('input[type=checkbox]'), completed);
+
+    });
 
   }
 
